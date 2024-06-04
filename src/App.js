abfb0c8342d917b0598bd3d6f24b016fec59287a
David111922 <salve62@yahoo.com>
2024-06-03 20:35:17 -0700

    craigsetup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-// Import data
+// ImportImport data
 
 // Import components
 import './App.css';
-
+import Cars from './postings.js
 function App(){
   return (
     <div>
       <h1>craigslist</h1>
       <div className="App">
-        {/* Your content will go here! */}
+        <App items={cars} />
       </div>
     </div>
   );
--- src/postings.js
+++ src/postings.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export const postings = [
     {
         title: "Car For Sale",
